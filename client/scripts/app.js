5178a66f2783f8b760ea035a993c2184d550be33
dookpham <dookpham@gmail.com>
2016-06-20 12:33:48 -0700

    Work on clearMessages method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -20,6 +20,29 @@ var app = {
     });
   },
 
+  fetch: function() {
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+      // url: 'https://api.parse.com/1/classes/messages',
+      type: 'GET',
+      // data: JSON.stringify(message),
+      // contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+
+  clearMessages: function() {
+    // grab the chats div and remove all children appended to it
+    $('#chats').remove();
+    //append chats back in
+    $('body').append('div').attr('id', 'chats');
+  }
 };
 
 
